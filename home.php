857ed6df07b9b750b1c3b6475af47357c7f42f61
Venkatesan05102002 <144411111+Venkatesan05102002@users.noreply.github.com>
2024-11-23 22:36:32 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,64 @@
+<?php include("head.html")
+?>
+<body>
+    <div class="home">
+        <div class="main">
+            <div class="left">
+                <h1> I'm Venkatesan</h1>
+                <p>This is my official portfolio website to show all <br>
+            Details and work experience web development.</p>
+            </div>
+        
+            <div class="right">
+                <img src="7.jpg" alt="photo"  height="200px" class="profile">
+            </div>
+        </div>
+        
+        <hr style="position:relative; z-index:1000; border-top: 4px solid black;">
+        <div class="about" id="about">
+            <h2 style="margin: 0;">About Me</h2>
+            <p>Hello! I'm venkatesan, a web developer. I specialized in web development.</p>
+            <p>My journey is Technology began with a curiosity for how things work, leading</p>
+            <p>me to explore the vast world in a more precise manner and I will be also </p>
+            <p>patients in work and will make the organizations grow better</p>
+        </div>
+        <div class="skill" id="skill">
+            <h2 class="skill1">Skills</h2>
+            <div class="skill1">
+                <h1 style="font-size:40px;">Technical Skill</h1><b>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>JavaScript</p>
+                <p>Python</p>
+                <p>Java</p>
+                <p>C</p>
+                <p>C++</p>
+                <p>pHp</p></b>
+            </div>
+            <div class="skill2">
+                <h1 style="font-size:40px;">Soft Skills</h1>
+                <b>
+                    <p>Problem Solving</p>
+                    <p>Teamwork</p>
+                    <p>Communication</p>
+                </b>
+            </div>
+            <div class="skill3">
+                <h1 style="font-size:40px;">Tools & Platforms</h1>
+                <b>
+                    <p>Git</p>
+                    <p>Linux</p>
+                    <p>OS Installation</p>
+                </b>
+            </div>
+        </div>
+
+        <div id="contact" class="contact">
+            <h2>Contact</h2>
+            <p>Phone No: 6379416846</p>
+            <p>Email: <a href="https://mail.google.com/mail/u/1/#inbox">venkatesanmahavai576.com</a></p>
+        </div>
+    </div>
+</body>
+<?php include("footer.html")
+?>
